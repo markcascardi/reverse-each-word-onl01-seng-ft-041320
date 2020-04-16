33da1ca904818c8d49ce13246d1a3d4dfa73cbe6
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 00:31:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+
+def reverse_each_word(sentence)
+array << sentence.split(" ")
+array.each { |word| word.reverse }
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  array.collect { |word| word.reverse }.join(" ")
+end
